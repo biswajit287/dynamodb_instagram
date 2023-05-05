ee86e0e810d968ac4d77b5d39e966692f03d2af1
Biswajit Mondal <biswajit.m@cloudbloom.io>
2023-05-05 18:00:52 +0530

    checking lambda function issue

---
+++ src/handlers/createUser.ts
@@ -0,0 +1,17 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent } from "aws-lambda"
+import { User, createUser, getUser } from "../data/user"
+
+export const main: APIGatewayProxyHandler = async (event: any) => {
+    const { username, name } = event.pathParameters
+    const user = new User(username, name)
+    await createUser(user)
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            user
+        })
+    }
+
+    return response
+}
